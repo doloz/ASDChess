3445371f8c6207504b21c507df978a837dd33fb4
Alex Doloz <doloz.alexander@gmail.com>
2015-09-02 19:44:33 +0600

    Add ASDChessGame

---
+++ ASDChess/ASDChessGame.swift
@@ -0,0 +1,15 @@
+//
+//  ASDChessGame.swift
+//  ASDChess
+//
+//  Created by Alex on 02.09.15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+import Foundation
+
+
+public class ASDChessGame {
+    var position: ASDPosition!
+    
+}
